657ecfce9cc236c6ee119b79c20a6226d3b0d29f
Rindho12 <rindo.ananta@gmail.com>
2016-12-02 13:08:21 +0800

    Rindho

---
+++ project/application/models/crud.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+* 
+*/
+class crud extends CI_Model
+{
+	public function get_all($table)
+	{
+		return $this->db->get($table);
+	}
+
+	public function get_one($table, $column_id, $id)
+	{
+		$this->db->where($column_id,$id);
+		return $this->db->get($table);
+	}
+
+	public function add($table, $data)
+	{
+		$this->db->insert($table,$data);
+	}
+
+	public function edit($table, $column_id, $id, $data)
+	{
+		$this->db->where($column_id,$id);
+		$this->db->update($table,$data);
+	}
+
+	public function delete($table, $column_id, $id)
+	{
+		$this->db->where($column_id, $id);
+		$this->db->delete($table);
+	}
+}
